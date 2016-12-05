731071e773790c1afefa961aa81c7856c27b7d12
WuTodd <wutob12450@outlook.com>
2016-12-05 20:47:30 +0800

    完成

--- BinarySearch/BinarySearch/main.cpp
+++ BinarySearch/BinarySearch/main.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+template <typename T>
+int BinarySearch(T key, T* datas, int l, int r);
 
 int main()
 {
-	
+	int a[] = { 1, 2, 3, 4, 5 };
+
+	cout << BinarySearch<int>(4, a, 0, 4);
+
+	system("pause");
 }
 
 template <typename T>
-int BinarySearch(T key, T[] datas, int l, int r)
+int BinarySearch(T key, T* datas, int l, int r)
 {
-	if (l == r || r < l)
-		return -1; //异常返回-1
+	if (r < l)
+		return -2; //异常返回-2
+	if (l == r)
+		return -1; //未找到返回-1
 
 	int mid = (r + l) / 2;
 
 	if (datas[mid] == key)
 		return mid;
 	if (datas[mid] < key)
-		return BinarySearch(mid, r);
+		return BinarySearch(key, datas, mid, r);
 	if (datas[mid] > key)
-		return BinarySearch(l, mid);
-
-
-
+		return BinarySearch(key, datas, l, mid);
 }
