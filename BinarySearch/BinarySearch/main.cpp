6d8a4ac9751a312a8623cea843596c9834973412
WuTodd <wutob12450@outlook.com>
2016-12-07 22:46:21 +0800

    fix bug

--- BinarySearch/BinarySearch/main.cpp
+++ BinarySearch/BinarySearch/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	int a[] = { 1, 2, 3, 4, 5 };
 
-	cout << BinarySearch<int>(4, a, 0, 4);
+	cout << BinarySearch<int>(6, a, 0, 4);
 
 	system("pause");
 }
@@ -19,15 +19,25 @@ int BinarySearch(T key, T* datas, int l, int r)
 {
 	if (r < l)
 		return -2; //异常返回-2
-	if (l == r)
-		return -1; //未找到返回-1
 
 	int mid = (r + l) / 2;
 
+	if (l == r && l == mid)
+		return -1; //未找到返回-1
 	if (datas[mid] == key)
-		return mid;
+		return mid; //找到返回坐标
 	if (datas[mid] < key)
-		return BinarySearch(key, datas, mid, r);
+		return BinarySearch(key, datas, mid + 1, r);
 	if (datas[mid] > key)
-		return BinarySearch(key, datas, l, mid);
+		return BinarySearch(key, datas, l, mid - 1);
+}
+
+int BinarySearch(int key, int* datas, int max)
+{
+	int l = 0, r = max, int mid;
+
+	for (mid = (l + r) / 2; !(l == r && l == mid); mid = (l + r) / 2)
+	{
+
+	}
 }
